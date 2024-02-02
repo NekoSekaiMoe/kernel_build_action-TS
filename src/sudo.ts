4c7f73f59a1ad6aecbaff12f8d13d8d72016e53a
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-02-02 23:01:00 +0800

    Fri Feb  2 23:01:00 CST 2024

--- src/sudo.ts
+++ src/sudo.ts
@@ -7,10 +7,10 @@ import * as exec from "@actions/exec";
 import * as process from "process";
 import * as cache from "@actions/cache";
 
-async function execBash(cmd : string) {
+export async function execBash(cmd : string) {
   await exec.exec("sh", ["-xc", cmd]);
 }
 
-async function execBashSudo(cmd : string) {
+export async function execBashSudo(cmd : string) {
   await execBash("$(which sudo) " + cmd);
 }
