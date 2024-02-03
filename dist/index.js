703b22318e7c1be2ee3d054a9ea447b5af89451d
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-02-03 15:07:41 +0800

    Sat Feb  3 15:07:40 CST 2024

--- dist/index.js
+++ dist/index.js
@@ -25962,13 +25962,17 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.KernelDir = exports.depth = exports.branch = exports.KernelUrl = exports.python2 = void 0;
+exports.KsuVersion = exports.ksu = exports.config = exports.arch = exports.KernelDir = exports.depth = exports.branch = exports.KernelUrl = exports.python2 = void 0;
 const core = __importStar(__nccwpck_require__(6904));
 exports.python2 = core.getInput('python2', { required: false });
 exports.KernelUrl = core.getInput('KernelUrl', { required: true });
 exports.branch = core.getInput('branch', { required: true });
 exports.depth = core.getInput('depth', { required: false });
 exports.KernelDir = core.getInput('KernelDir', { required: false });
+exports.arch = core.getInput('arch', { required: true });
+exports.config = core.getInput('config', { required: false });
+exports.ksu = core.getInput('ksu', { required: false });
+exports.KsuVersion = core.getInput('KsuVersion', { required: false });
 
 
 /***/ }),
@@ -26013,6 +26017,65 @@ async function InstallDep() {
     }
 }
 exports.InstallDep = InstallDep;
+
+
+/***/ }),
+
+/***/ 1803:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.InitKSU = void 0;
+const exec = __importStar(__nccwpck_require__(9009));
+const io = __importStar(__nccwpck_require__(5036));
+const input_1 = __nccwpck_require__(671);
+async function InitKSU() {
+    if (input_1.ksu === 'true') {
+        console.log("Initializing KsernelSU");
+        const directoryPath = `./kernel/${input_1.KernelDir}`;
+        const ksuConfig = `${directoryPath}/KernelSU/Kconfig`;
+        if (await io.which(ksuConfig)) {
+            console.log("KernelSU is Initialized.Skipping.");
+            return;
+        }
+        try {
+            const command = `curl -SsL https://github.com/tiann/KernelSU/raw/main/kernel/setup.sh | bash -s ${input_1.KsuVersion}`;
+            const options = {
+                cwd: directoryPath,
+            };
+            await exec.exec(command, [], options);
+        }
+        finally {
+            console.log("Ksu");
+        }
+    }
+}
+exports.InitKSU = InitKSU;
 
 
 /***/ }),
@@ -26081,10 +26144,12 @@ exports.run = void 0;
 const swap_1 = __nccwpck_require__(2399);
 const install_dep_1 = __nccwpck_require__(399);
 const python2_1 = __nccwpck_require__(9303);
+const ksu_1 = __nccwpck_require__(1803);
 async function run() {
-    (0, swap_1.swap)();
-    (0, install_dep_1.InstallDep)();
-    (0, python2_1.InstallPython2)();
+    await (0, swap_1.swap)();
+    await (0, install_dep_1.InstallDep)();
+    await (0, python2_1.InstallPython2)();
+    await (0, ksu_1.InitKSU)();
 }
 exports.run = run;
 
--- src/ksu.ts
+++ src/ksu.ts
@@ -1,23 +1,31 @@
 import * as os from "os";
 import * as fs from 'fs';
 import * as path from 'path';
-import { exec } from 'child_process';
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
-import { ksu } from './input';
-import { KernelDir } from './input';
-import { KsuVersion } from './run';
-import { execBash } from './sudo'
+import * as io from "@actions/io";
+import { ksu, KernelDir, KsuVersion } from './input';
 
 export async function InitKSU(): Promise<void> {
     if (ksu === 'true') {
-      console.log(" Initializing KsernelSU");
-      const directoryPath = './kernel/${KernelDir}';
-      exec(`curl -SsL https://github.com/tiann/KernelSU/raw/main/kernel/setup.sh | bash -s ${KsuVersion}}`, { cwd: directoryPath }, (error, stdout, stderr) => {
-      if (error) {
-          console.error(`exec error: ${error}`);
-          return 1;
-      }
+        console.log("Initializing KsernelSU");
+        const directoryPath = `./kernel/${KernelDir}`;
+        const ksuConfig = `${directoryPath}/KernelSU/Kconfig`;
+        if (await io.which(ksuConfig)) {
+            console.log("KernelSU is Initialized.Skipping.");
+            return;
+        }
+        try {
+            const command = `curl -SsL https://github.com/tiann/KernelSU/raw/main/kernel/setup.sh | bash -s ${KsuVersion}`;
+
+            const options = {
+                cwd: directoryPath,
+            };
+            await exec.exec(command, [], options);
+        }
+
+        finally{
+           console.log("Ksu");
+        }
     }
-  }
 }
