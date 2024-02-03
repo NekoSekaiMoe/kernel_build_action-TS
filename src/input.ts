e7010c4bbfacde5905b49da6791c643a0bf8a25b
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-03 13:38:17 +0800

    Update input.ts

--- src/input.ts
+++ src/input.ts
@@ -12,3 +12,10 @@ export const KernelUrl = core.getInput('KernelUrl', { required: true });
 export const branch = core.getInput('branch', { required: true });
 export const depth = core.getInput('depth', { required: false });
 export const KernelDir = core.getInput('KernelDir', { required: false });
+export const arch = core.getInput('arch', { required: true });
+export const config = core.getInput('config', { required: false });
+export const ksu = core.getInput('ksu', { required: false });
+export const KsuVersion = core.getInput('KsuVersion', { required: false });
+
+
+
