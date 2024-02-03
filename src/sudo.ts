f0ccef1e51cf8cbe7cf6c4e73954e9cf99d79d93
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-03 11:07:27 +0800

    sudo.ts: delete unnecessary dependices

--- src/sudo.ts
+++ src/sudo.ts
@@ -1,11 +1,8 @@
 import fs from "fs";
 import os from "os";
-import path from "path";
 import * as core from "@actions/core";
 import * as io from "@actions/io";
 import * as exec from "@actions/exec";
-import * as process from "process";
-import * as cache from "@actions/cache";
 
 export async function execBash(cmd : string) {
     await exec.exec("bash", ["-xc", cmd]);
