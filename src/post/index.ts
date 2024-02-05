e5bc76772f9dc9cbd36ce1c478460905f04afc42
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-06 00:23:02 +0800

    post: index.ts: fix clang version

--- src/post/index.ts
+++ src/post/index.ts
@@ -3,7 +3,13 @@ import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 
 async function post(): Promise<void> {
-    await exec.exec("echo 6");
+    if (await io.which("$HOME/gcc-64/bin")) {
+        await exec.exec("HOME/gcc-64/bin/aarch64-linux-android-as --version");
+        return;
+    } else if (await io.which("$HOME/clang/bin/clang")) {
+        await exec.exec("$HOME/clang/bin/clang --version");
+        return;
+    }
 }
 
 (async () => {
