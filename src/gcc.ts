a56a6a6d661afd8e36493caeca1322bb504afd14
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-06 00:03:02 +0800

    gcc.ts: fix typo

--- src/gcc.ts
+++ src/gcc.ts
@@ -18,7 +18,7 @@ export async function InstallGcc(): Promise<void> {
             execDown(https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -o gcc-aarch64.tar.gz);
             execBash(tar -C $HOME/gcc-64 -zxf gcc-aarch64.tar.gz);
             execDown(https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -o gcc-aarch -o gcc-arm.tar.gz);
-            execBash(tar -C $HOME/gcc-32 -zxf gcc-arm.tar.gz)
+            execBash(tar -C $HOME/gcc-32 -zxf gcc-arm.tar.gz);
         } else {
             execBash(git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/ --depth=1 -b android${AndroidVersion}-release $HOME/gcc-64);
             execBash(git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/ --depth=1 -b android${AndroidVersion}-release $HOME/gcc-32);
