aff3ec25a6b979dd567612c0aec2037d18a26f11
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-07 15:40:50 +0800

    apatch.ts: chore: Initialize

---
+++ src/apatch.ts
@@ -0,0 +1,31 @@
+import * as os from "os";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import { apatch, KernelDir, config, arch } from './input';
+
+export async function InitApatch(): Promise<void> {
+    if (apatch === 'true') {
+        console.log("Initializing APatch");
+        const directoryPath = `./kernel/${KernelDir}`;
+        const ApatchConfig = `${directoryPath}/Apatch/Kconfig`;
+        if (await io.which(ApatchConfig)) {
+            console.log("Apatch is Initialized.Skipping.");
+            return;
+        }
+        try {
+            const command = `mkdir drivers/apatch && aria2c https://github.com/dabao1955/kernel_build_action/raw/main/apatch/fix_module.patch && git apply fix_module.patch`;
+
+            const options = {
+                cwd: directoryPath,
+            };
+            await exec.exec(command, [], options);
+        }
+
+        finally{
+           await exec.exec("echo CONFIG_APATCH_SUPPORT=y >>${KernelDir}/arch/${arch}/${config}");
+        }
+    }
+}
