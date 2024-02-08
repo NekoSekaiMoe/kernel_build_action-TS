c3650d2333ec1953e91cdae1f2777ae0f5b8be1b
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-02-08 13:13:39 +0800

    compile.ts: feat: Initialize
    Signed-off-by: SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>

---
+++ src/compile.ts
@@ -0,0 +1,5 @@
+import { execBash } from './sudo'
+import { arch, config, directoryPath, ccache, AospClang, AospGcc, OtherClang } from './input'
+
+export async function compile(): Promise<void> {
+}
