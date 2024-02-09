26a9b3415398321cad8468f26d88aaef5ece4fa0
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-02-09 11:57:47 +0800

    run.ts: faet: add cleanup

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,8 @@
+import { cleanup } from './cleanup';
 import { swap } from './swap';
 import { InstallDep } from './install-dep';
 import { InstallPython2 } from './python2';
-import { PullCode } from "./code";
+import { PullCode } from './code';
 import { InstallGcc } from "./gcc";
 import { InitKSU } from './ksu';
 import { InitApatch } from './apatch';
