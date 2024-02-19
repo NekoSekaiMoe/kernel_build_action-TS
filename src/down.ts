b2e6c3a254a1479f56761f164fbafe04f2d63fe5
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-02-19 15:17:45 +0800

    down.ts: feat: Initialize
    Signed-off-by: SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>

---
+++ src/down.ts
@@ -0,0 +1,9 @@
+import { downUrl, downPath } from './input';
+import { execBash } from './input';
+
+export const fileUrl = `${downUrl}`;
+export const outputPath = `${downPath}`;
+
+downloadFile(fileUrl, outputPath)
+    .then(() => execBash('echo File downloaded successfully.'))
+    .catch(e => console.error('Download failed:', e));
