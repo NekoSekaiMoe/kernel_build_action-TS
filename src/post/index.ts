ad94e010d23140024c65b17d61a31a8eb17d9193
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-09-02 17:58:58 +0800

    Update index.ts

--- src/post/index.ts
+++ src/post/index.ts
@@ -3,6 +3,10 @@ import * as  io from "@actions/io";
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 
+import { saveCache } from "@actions/cache";
+import { getState, warning } from "@actions/core";
+import { CacheState } from "./action";
+
 async function post(): Promise<void> {
     if (await io.which(os.homedir() + "/gcc-64/bin")) {
         await exec.exec("HOME/gcc-64/bin/aarch64-linux-android-as --version");
@@ -14,6 +18,14 @@ async function post(): Promise<void> {
         await console.error("Failed to load compiler.");
         process.exit(127);
     }
+    const state: CacheState = JSON.parse(getState("cache"));
+        if (state.cacheEnabled && !state.cacheHit) {
+            try {
+                await saveCache([state.Path], state.url);
+                process.exit(0);
+            } catch (error) {
+                warning("Failed to save cache.");
+         }
 }
 
 (async () => {
